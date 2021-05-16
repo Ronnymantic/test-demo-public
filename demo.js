bde732a57d8aa82ca278f2307b7b69eae58c4b20
Ronnymantic <1533198196@qq.com>
2021-05-16 15:50:15 +0800

    第一次上传代码

---
+++ demo.js
@@ -0,0 +1,11 @@
+/*
+	2021-0516-1540
+	ronny
+	demo.js
+*/
+window.onload = function() {
+	let box = document.getElementsByClassName('box')[0]
+	box.addEventListener("click", () => {
+		alert("这是一段测试js")
+	})
+}
